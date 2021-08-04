fbe432e8ed72a242565992cc0ad11171e95fd1e9
mauricio pineda <map1900@outlook.com>
2021-08-04 16:10:33 -0400

    third silo in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index'
-
+  get "/tasks", to: "tasks#index"
+  get "/tasks/:id", to: "tasks#show", as: :task
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
+  
 
 
 #   Prefix Verb   URI Pattern                     Controller#Action
