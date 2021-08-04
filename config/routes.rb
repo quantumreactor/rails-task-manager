b706a1a395efafc5945ef715d85493d0f0801bcc
mauricio pineda <map1900@outlook.com>
2021-08-04 15:40:18 -0400

    first silo created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index'
+
+
+
+#   Prefix Verb   URI Pattern                     Controller#Action
+#   restaurants GET    /restaurants(.:format)          restaurants#index
+#               POST   /restaurants(.:format)          restaurants#create
+# new_restaurant GET    /restaurants/new(.:format)      restaurants#new
+# edit_restaurant GET    /restaurants/:id/edit(.:format) restaurants#edit
+#    restaurant GET    /restaurants/:id(.:format)      restaurants#show
+#               PATCH  /restaurants/:id(.:format)      restaurants#update
+#               DELETE /restaurants/:id(.:format)      restaurants#destroy
+
 end
