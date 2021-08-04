abeb137de64d29e7622cce9ca63ce27cfda3ac0c
mauricio pineda <map1900@outlook.com>
2021-08-04 18:01:56 -0400

    almost done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -19,13 +19,16 @@ class TasksController < ApplicationController
 
   def edit
     # self.show #ya pero no de esta manera, unforeseen consequences
-    @task = Task.find(params[:id])
   end
 
-  def update 
-    @task = Task.find(params[:id])
+  def update
     @task.update(task_params)
     redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
   end
 
 
@@ -34,6 +37,10 @@ class TasksController < ApplicationController
   # only YOU can prevent hackers/crackers
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 
 end
